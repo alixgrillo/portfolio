a888dd7a765c8c8c8385e781576023f0b980d013
Alix Grillo <alix.grillo2+github@gmail.com>
2019-11-29 19:11:34 -0700

    added slick carousel for portfolio section

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # portfolio
 Updated Portfolio
-Photo by Alex Knight on Unsplash
+Photo by Alex Knight on Unsplash (background)
+
+Photo by Kira auf der Heide on Unsplash (gift)
+
+Photo by Artem Maltsev on Unsplash (harry potter)
+
+Photo by Ruby Schmank on Unsplash (animals gif)
+
+Photo by Jeremy Yap on Unsplash (movie trivia)
--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,2 +1,39 @@
-$('#textarea1').val('New Text');
-  M.textareaAutoResize($('#textarea1'));
+// $('#textarea1').val('New Text');
+//   M.textareaAutoResize($('#textarea1'));
+
+
+$('.portfolio-carousel').slick({
+    dots: true,
+    // fade: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
