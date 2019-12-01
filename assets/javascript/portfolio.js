7b7d2fc72fb445f3376659451b01d57a4be50432
Alix Grillo <alix.grillo2+github@gmail.com>
2019-12-01 12:42:21 -0700

    updated landing page with buttons to social media

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -12,28 +12,39 @@ $('.portfolio-carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
     ]
-  });
+});
+
+$(document).ready(function(){
+    $(".btn").click(function() {
+        switch(this.id) {
+            case 'download':window.open('assets/documents/cv-Alexandra_Clare.pdf'); break;
+            case 'github': window.open('https://github.com/alixgrillo'); break;
+            case 'linkedin': window.open('https://www.linkedin.com/in/alixclare/'); break;
+            case 'hireme': window.location.href = "#contact"; break;
+        }
+    });
+})
